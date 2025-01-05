ec7a5841a9b0cba7aedd3f7697f4c37e212d5d7e
uday-hub <udaymore747@gmail.com>
2025-01-05 14:56:52 +0530

    one change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         </div>
         <div className="home-text-section">
           <h1 className="primary-heading">
-          Your Favourite Food Delivered Hot & Fresh Uday
+          Your Favourite Food Delivered Hot & Fresh Uday more
           </h1>
           <p className="primary-text">
           Healthy switcher chefs do all the prep work, like peeding, chopping
